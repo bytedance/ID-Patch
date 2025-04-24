9651cb756be86bb02845392e03a0215d8cea25f1
damon-demon <zhan1853@msu.edu>
2025-04-24 16:47:01 -0400

    refine demo

--- demo.py
+++ demo.py
@@ -1,6 +1,7 @@
 # Copyright 2024 Bytedance Ltd. and/or its affiliates
 # SPDX-License-Identifier: Apache-2.0
 
+import os
 import argparse
 from pathlib import Path
 import numpy as np
@@ -29,11 +30,14 @@ def extract_face_emb(arcface_encoder, cropped_face):
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--subject_dir', default='data/subjects')
-    parser.add_argument('--output_path', default='result.png')
-    parser.add_argument('--reference_image_path', default='data/reference_image.png')
+    # id-patch settings
+    parser.add_argument('--pose_image_path', default='data/poses/example_pose.png')
+    parser.add_argument('--subject_dir', default='data/subjects') 
+    parser.add_argument('--subjects', default='exp_man.jpg,exp_woman.jpg')  # comma separated list of subject images in order (from left to right)
     parser.add_argument('--prompt', default="a young couple in front of their burning home still managing to find a moment of joy amidst disaster. cheerfully raise glasses filled with a bright blue drink")
     parser.add_argument('--negative_prompt', default="nude, worst quality, low quality, normal quality, nsfw, abstract, glitch, deformed, mutated, ugly, disfigured, text, watermark, bad hands, error, jpeg artifacts, blurry, missing fingers")
+    
+    # inference hyperparameters
     parser.add_argument('--guidance_scale', type=float, default=5.5)
     parser.add_argument('--controlnet_conditioning_scale', type=float, default=0.8)
     parser.add_argument('--num_inference_steps', type=int, default=50)
@@ -41,14 +45,24 @@ if __name__ == '__main__':
     parser.add_argument('--seed', type=int, default=1234)
     parser.add_argument('--idp_model_path', default='models/ID-Patch')
     parser.add_argument('--base_model_path', default='RunDiffusion/Juggernaut-X-v10')
+
+    # output settings
+    parser.add_argument('--output_dir', default='results')
+    parser.add_argument('--output_name', default='exp_result')
     args = parser.parse_args()
     
     tf.config.set_visible_devices([], 'GPU')
     mtcnn_inferencer = MTCNN() # MTCNN might be slow, could be replaced by other face detectors, as long as it provides 5 keypoints
-    # load subjects
+
+    # create output directory
+    os.makedirs(args.output_dir, exist_ok=True)
+
+    # load subjects in order (from left to right)
     face_embs = []
     arcface_encoder = facexlib.recognition.init_recognition_model('arcface', device='cuda:0')
-    for subject_path in sorted(list(Path(args.subject_dir).iterdir())):
+    subjects = args.subjects.split(',')
+    for subject in subjects:
+        subject_path = os.path.join(args.subject_dir, subject)
         image_subject = cv2.imread(str(subject_path))
         mtcnn_subject = mtcnn_inferencer.detect_faces(image_subject[:,:,::-1])
         kps_subject = mtcnn_to_kps(mtcnn_subject)
@@ -56,8 +70,8 @@ if __name__ == '__main__':
         face_embs.append(extract_face_emb(arcface_encoder, cropped_face_subject))
     face_embs = torch.stack(face_embs)
     
-    # load style
-    image_reference = cv2.imread(args.reference_image_path)
+    # load pose
+    image_reference = cv2.imread(args.pose_image_path)
     mtcnn_reference = mtcnn_inferencer.detect_faces(image_reference[:,:,::-1])
 
     # estimate pose
@@ -67,6 +81,7 @@ if __name__ == '__main__':
     
     idpatch_inferencer = IDPatchInferencer(base_model_path=args.base_model_path, idp_model_path=args.idp_model_path)
     control_image = Image.fromarray(draw_openpose_from_mmpose(image_reference * 0, keypoints, scores))
+    control_image.save(os.path.join(args.output_dir, args.output_name + '_pose.png'))
 
     result = idpatch_inferencer.generate(
         face_embs,
@@ -80,4 +95,5 @@ if __name__ == '__main__':
         id_injection_ratio=args.id_injection_ratio,
         seed=args.seed
     )
-    result.save(args.output_path)
+    result.save(os.path.join(args.output_dir, args.output_name + '_img.png'))
+    
